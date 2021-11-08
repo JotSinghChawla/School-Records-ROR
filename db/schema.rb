7d8224a23eeee4ee53a38aed31af7b4483f8b724
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-08 21:29:44 +0530

    Added 'devise' for authentication and basic login & signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_023324) do
+ActiveRecord::Schema.define(version: 2021_11_07_170025) do
 
   create_table "student_classes", force: :cascade do |t|
     t.integer "class_number"
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2021_11_01_023324) do
     t.datetime "updated_at", null: false
     t.integer "rollnumber"
     t.index ["student_class_id"], name: "index_students_on_student_class_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
